3ab1ec587d1c5ceb7a02ad8f049af453c99b92be
Elliott Roche <eroche91@gmail.com>
2016-12-10 12:33:01 -0600

    accounting for possible negative digit when reading thermometer value (#3)

--- lib/one_wire/thermometer.rb
+++ lib/one_wire/thermometer.rb
@@ -1,13 +1,13 @@
 module OneWire
   class Thermometer < Base
-    
+
     PREFIX = %w{10 22 28 3B}
 
     attr_reader :last_value
 
     def value
       @last_value = @value
-      @value = w1_slave[/t=(\d*)/, 1].to_f / 1000
+      @value = w1_slave[/t=(-?\d*)/, 1].to_f / 1000
     end
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe OneWire do
-  before do 
+  before do
     allow(File).to receive(:join).and_call_original
     allow(File).to receive(:join).with(File::SEPARATOR, 'sys', 'bus', 'w1', 'devices', '*-*').and_return(File.expand_path "#{__dir__}/fixtures/sys_bus_w1_devices/*-*")
   end
@@ -22,7 +22,7 @@ describe OneWire do
   describe OneWire::Base do
     it { is_expected.to be_kind_of OneWire::Base }
     it { expect{ subject.name }.to raise_error Errno::ENOENT }
-    
+
     let(:empty_device) {OneWire::Base.new(OneWire.find(/00-/).first)}
     it { expect(empty_device.name).to eq "00-000000000000" }
     xit { expect(empty_device.id).to eq "" } # don't know how to read `id` file
@@ -32,22 +32,41 @@ describe OneWire do
     it { expect { empty_device.last_value }.to raise_error(NotImplementedError) }
   end
 
-  describe OneWire::Thermometer do    
+  describe OneWire::Thermometer do
     subject {OneWire::Thermometer.new(OneWire.find(/-000000000003/).first)}
 
     it { is_expected.to be_kind_of OneWire::Thermometer }
     it { expect(subject.name).to eq "28-000000000003" }
     xit { expect(subject.id).to eq "" } # don't know how to read `id` file
     it { expect(subject.w1_slave).to eq "9f 01 4b 46 7f ff 01 10 40 : crc=40 YES\n9f 01 4b 46 7f ff 01 10 40 t=25937" }
-    
+
     it { expect(subject.value).to eq 25.937 }
 
-    it "should update the last value" do 
+    it "should update the last value" do
       expect(subject.last_value).to be_nil
       subject.value
       expect(subject.last_value).to be_nil
       subject.value
       expect(subject.last_value).to eq 25.937
+    end
+  end
+
+  describe OneWire::Thermometer do
+    subject {OneWire::Thermometer.new(OneWire.find(/28-000000000004/).first)}
+
+    it { is_expected.to be_kind_of OneWire::Thermometer }
+    it { expect(subject.name).to eq "28-000000000004" }
+    xit { expect(subject.id).to eq "" } # don't know how to read `id` file
+    it { expect(subject.w1_slave).to eq "9f 01 4b 46 7f ff 01 10 40 : crc=40 YES\n9f 01 4b 46 7f ff 01 10 40 t=-25937" }
+
+    it { expect(subject.value).to eq -25.937 }
+
+    it "should update the last value" do
+      expect(subject.last_value).to be_nil
+      subject.value
+      expect(subject.last_value).to be_nil
+      subject.value
+      expect(subject.last_value).to eq -25.937
     end
   end
 end
